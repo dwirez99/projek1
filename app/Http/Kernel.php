61949699f7f60028130d701b7a8c3980b127978a
dwirez <dwirez64@gmail.com>
2025-06-20 15:21:45 +0700

    API

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,10 +1,29 @@
 <?php
 // Add to the $middleware property in app/Http/Kernel.php
-protected $middleware = [
-    // ...existing middleware
-    \App\Http\Middleware\SecureHeaders::class,
-];
+namespace App\Http;
 
-function ensureHttps($url) {
-    return str_replace('http://', 'https://', $url);
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        // ...existing middleware
+        \App\Http\Middleware\SecureHeaders::class,
+    ];
+
+    protected $middlewareGroups = [
+        'web' => [
+            // ... existing middleware
+        ],
+
+        'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    public function ensureHttps($url) {
+        return str_replace('http://', 'https://', $url);
+    }
 }
